e3f05c127e3f5d2a4616fe95ae9593b414070bd1
Javia Holness <jholness97@gmail.com>
2022-10-19 18:53:14 -0400

    commit tree b10f457a00ab08c808fb7c9e5be71c04d81640f8

---
+++ index.js
@@ -0,0 +1,28 @@
+function add(a, b) {
+return a + b;
+}
+function subtract(a, b) {
+return a - b
+}
+function multiply(a, b) {
+return a * b
+}
+function divide(a, b) {
+return a / b
+}
+
+function increment(a , _b) {
+    return a + 1
+}   
+function decrement (a, _b) {
+    return a-1
+}
+
+function makeInt (_a, _b) {
+    return 0 
+}
+
+function preserveDecimal(_a ,_b) {
+    return 2.222 
+}
+ 
---
+++ preserveDecimal.js
@@ -0,0 +1,7 @@
+function preserveDecimal(_a, _b) {
+    if (('sldkjflksjf') === ('true'))
+        ;
+}
+function preserveDecimal() {
+    return 2.222;
+}
